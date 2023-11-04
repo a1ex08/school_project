0ecdba88f8d122861d65149d0e126131f2594733
Alexandr Chizhov <a1ex.chizhov@yandex.ru>
2023-11-04 21:00:19 +0700

    Настройка админ-панели

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status'] # Список отображаемых полей
+    list_filter = ['status', 'created', 'publish', 'author']        # Фильтры по полям
+    search_fields = ['title', 'body']                               # Поля по которым осуществляется текстовый поиск
+    prepopulated_fields = {'slug': ('title',)}                      # Формирование слага из указанных полей
+    raw_id_fields = ['author']                                      # Более удобное меню выбора плльзователя
+    date_hierarchy = 'publish'                                      # Навигация по иерархии дат
+    ordering = ['status', 'publish']                                # Порядок сортировки при отображении постов
